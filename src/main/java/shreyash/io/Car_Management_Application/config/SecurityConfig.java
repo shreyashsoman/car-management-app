f4fc9d104cd0ac9860fa15b9a47ca86ce62ebfdf
aditya <avats218@gmail.com>
2024-11-14 15:42:08 +0530

    Updated security

--- src/main/java/shreyash/io/Car_Management_Application/config/SecurityConfig.java
+++ src/main/java/shreyash/io/Car_Management_Application/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,6 +25,12 @@ public class SecurityConfig {
                 .httpBasic(httpBasic -> httpBasic.realmName("Car Management Application")); // Configure HTTP Basic authentication
 
         return http.build();
+    }
+
+    // Define a PasswordEncoder bean so that Spring can inject it wherever needed
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
 
