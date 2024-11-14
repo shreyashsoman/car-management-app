ed31636882fce91ac52c94e32f1891ed10a7b4d7
aditya <avats218@gmail.com>
2024-11-14 16:59:47 +0530

    Swagger config class

--- src/main/java/shreyash/io/Car_Management_Application/config/SecurityConfig.java
+++ src/main/java/shreyash/io/Car_Management_Application/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html",
+                                "/swagger-ui/index.html",
                                 "/swagger-resources/**",
                                 "/webjars/**"
                         ).permitAll() // Allow access to Swagger UI without authentication
